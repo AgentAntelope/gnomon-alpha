1ea5fbb32818cabe5d7fbf3b93cab2ec1c51c262
Alex Sunderland <agentantelope@gmail.com>
2016-08-25 00:33:22 +0100

    Make it glitch

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'gnomon#index'
+  get '/:first', to: 'gnomon#index', as: 'one'
+  get '/:first/:second', to: 'gnomon#index', as: 'two'
+  get '/:first/:second/:third', to: 'gnomon#index', as: 'three'
+  get '/:first/:second/:third/:fourth', to: 'gnomon#index', as: 'four'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
